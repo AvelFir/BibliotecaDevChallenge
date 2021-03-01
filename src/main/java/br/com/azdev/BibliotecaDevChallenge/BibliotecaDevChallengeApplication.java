90209201ba264225088e519c308a44ddd8e42e6a
AvelFir <guichasemartinsgmr@hotmail.com>
2021-03-01 02:40:27 -0300

    initial commit

---
+++ src/main/java/br/com/azdev/BibliotecaDevChallenge/BibliotecaDevChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.azdev.BibliotecaDevChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaDevChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaDevChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/azdev/BibliotecaDevChallenge/BibliotecaDevChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.azdev.BibliotecaDevChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaDevChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
